f257ff4e0e78a8b4aaaf957682550e4f17f70038
KIMYO9 <fgh33678@gmail.com>
2022-09-23 22:18:31 +0900

    widget 기초 사이즈 설정

--- MainGui.py
+++ MainGui.py
@@ -8,11 +8,36 @@ class MainWidget(QWidget):
         self.setWindowTitle('GSHFD')
 
         self.main_layout = QGridLayout()
-        self.left_layout = QHBoxLayout(self.main_layout)
+        self.left_layout = QHBoxLayout()
 
         self.link_set_button = QPushButton('주소추가', parent=self.left_layout)
         self.change_account_button = QPushButton('계정번경', parent=self.left_layout)
         self.start_button = QPushButton('실행', parent= self.left_layout)
 
         # 왼쪽 주소 창 -> 객체화 -> main_layout ( 0, 1 ) 위치 예정
-        
+        self._init_widget()
+        self._init_layout()
+
+    def _init_widget(self):
+        # linke set button
+        self.link_set_button.setFixedSize(QSizePolicy.Minimum, QSizePolicy.Minimum)
+
+        # change account button
+        self.change_account_button.setFixedSize(QSizePolicy.Minimum, QSizePolicy.Minimum)
+
+        # start button
+        self.start_button.setFixedSize(QSizePolicy.Minimum, QSizePolicy.Minimum)
+    
+    def _init_layout(self):
+        self.left_layout.addWidget(self.link_set_buttonm, 1)
+        self.left_layout.addWidget(self.change_account_button, 1)
+        self.left_layout.addSpacerItem(QSpacerItem(1,100))
+        self.left_layout.addWidget(self.start_button, 1)
+
+def open_main_widget():
+    app = QApplication(sys.argv)
+    widget = MainWidget()
+    widget.show()
+    sys.exit(app.exec_)
+
+open_main_widget()
