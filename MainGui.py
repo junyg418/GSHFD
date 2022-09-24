f8c21542dc86fadcbca75800dd131b2c5968aa76
KIMYO9 <fgh33678@gmail.com>
2022-09-24 21:52:23 +0900

    scroll, mainlayout 세팅

--- MainGui.py
+++ MainGui.py
@@ -14,6 +14,9 @@ class MainWidget(QWidget):
         self.change_account_button = QPushButton('계정번경', parent=self.left_layout)
         self.start_button = QPushButton('실행', parent= self.left_layout)
 
+        self.link_scroll_area = QScrollArea()
+        self.link_widget = LinkWidget()
+        
         # 왼쪽 주소 창 -> 객체화 -> main_layout ( 0, 1 ) 위치 예정
         self._init_widget()
         self._init_layout()
@@ -29,10 +32,25 @@ class MainWidget(QWidget):
         self.start_button.setFixedSize(QSizePolicy.Minimum, QSizePolicy.Minimum)
     
     def _init_layout(self):
+        # main layout
+        self.main_layout.addLayout(self.left_layout, 0, 0)
+        self.main_layout.addWidget(self.link_scroll_area, 1, 0)
+
+        # left layout
         self.left_layout.addWidget(self.link_set_buttonm, 1)
         self.left_layout.addWidget(self.change_account_button, 1)
         self.left_layout.addSpacerItem(QSpacerItem(1,100))
         self.left_layout.addWidget(self.start_button, 1)
+        
+    
+    def set_link_scroll_widget(self):
+        self.link_scroll_area.setWidget(self.link_widget)
+
+
+class LinkWidget(QWidget):
+    def __init__(self):
+        super().__init__()
+
 
 def open_main_widget():
     app = QApplication(sys.argv)
