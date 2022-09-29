17f4cca79428518eaeb36bc94e7a603ae28e64d1
KIMYO9 <fgh33678@gmail.com>
2022-09-29 23:15:30 +0900

    CsvDataProcessingModule.py 생성 및 to list 함수 작성

---
+++ CsvDataProcessingModule.py
@@ -0,0 +1,21 @@
+import pandas as pd
+
+
+def get_user_data():
+    user_data_df = pd.read_csv('./csv_file/user_data.csv')
+    return user_data_df
+
+
+def get_link_to_list() -> None:
+    """
+    user_data.csv 파일에서 link 를 추출 후
+    :return:
+    """
+    user_data_df = get_user_data()
+    link_df = user_data_df['link']
+    link_list = link_df.values.tolist()
+    return link_list
+
+
+if __name__ == '__main__':
+    print(get_link_to_list())
