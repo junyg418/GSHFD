eb4bd61eb6f23bdc65c38abdd78f163fc99d466c
KIMYO9 <fgh33678@gmail.com>
2022-09-30 10:30:06 +0900

    주석 처리 및 타입힌트 작성

--- CsvDataProcessingModule.py
+++ CsvDataProcessingModule.py
@@ -1,15 +1,21 @@
 import pandas as pd
 
 
-def get_user_data():
+def get_user_data() -> pd.DataFrame:
+    """
+    user_data.csv 파일을 불러와 데이터 프레임으로 변경하는 함수
+    :return:
+        pandas.DataFrame : user_data.csv -> DataFrame
+    """
     user_data_df = pd.read_csv('./csv_file/user_data.csv')
     return user_data_df
 
 
-def get_link_to_list() -> None:
+def get_link_to_list() -> list:
     """
-    user_data.csv 파일에서 link 를 추출 후
+    user_data.csv 파일에서 link 를 추출 후 리스트로 변경하여 반환하는 함수
     :return:
+        user_data.csv -> links -> list
     """
     user_data_df = get_user_data()
     link_df = user_data_df['link']
