851cabae2162983ab0d6494e7e12a01a077b1800
KIMYO9 <fgh33678@gmail.com>
2022-10-06 23:10:06 +0900

    시그널 추가 및 label추가

--- AddUserDataGui.py
+++ AddUserDataGui.py
@@ -5,19 +5,27 @@ import sys
 class AddUserWidget(QWidget):
     def __init__(self):
         super(AddUserWidget, self).__init__()
+        self.setWindowTitle('주소 추가')
         self.main_layout = QGridLayout()
 
+        self.descriptive_label = QLabel()
         self.add_link_lineedit = QLineEdit()
         self.accept_button = QPushButton('추가')
 
+        self._init_widget()
         self._init_ui()
 
     def _init_ui(self):
         self.setLayout(self.main_layout)
 
-        self.main_layout.addWidget(self.add_link_lineedit, 0, 0, 1, 4)
-        self.main_layout.addWidget(self.accept_button, 1, 3, 1, 1)
+        self.main_layout.addWidget(self.descriptive_label, 0, 0)
+        self.main_layout.addWidget(self.add_link_lineedit, 1, 0, 1, 4)
+        self.main_layout.addWidget(self.accept_button, 2, 3, 1, 1)
 
+    def _init_widget(self):
+        self.descriptive_label.setText('형식에 맞추어 작성해주세요. form: https://youtube.com')
+
+        self.accept_button.clicked.connect(self.clicked_accept_button)
     def get_written_link(self):
         return self.add_link_lineedit.text()
     
